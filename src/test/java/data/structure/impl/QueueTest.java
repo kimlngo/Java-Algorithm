74870024e2db72560040ebf40bf3874088d1bb6a
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-11-20 13:13:04 -0500

    More UTs

--- src/test/java/data/structure/impl/QueueTest.java
+++ src/test/java/data/structure/impl/QueueTest.java
@@ -1,12 +1,13 @@
 package data.structure.impl;
 
+import data.structure.api.IQueue;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueTest {
     @Test
-    void testEnqueueAndDequeue() {
+    void testEnqueueAndDequeue_WithString() {
         Queue<String> sut = new Queue<>();
         sut.enqueue("FIRST");
         sut.enqueue("SECOND");
@@ -22,5 +23,26 @@ class QueueTest {
         assertNull(sut.dequeue());
         assertEquals(0, sut.size);
 
+    }
+
+    @Test
+    void testEnqueueAndDequeue_WithInteger() {
+        Queue<Integer> intQueue = new Queue<>();
+
+        intQueue.enqueue(Integer.valueOf(23));
+        intQueue.enqueue(Integer.valueOf(56));
+        intQueue.enqueue(Integer.valueOf(79));
+        intQueue.enqueue(Integer.valueOf(85));
+
+        intQueue.print();
+
+        assertEquals(4, intQueue.size);
+        assertEquals(23, intQueue.dequeue());
+        assertEquals(56, intQueue.dequeue());
+        assertEquals(79, intQueue.dequeue());
+        assertEquals(85, intQueue.dequeue());
+
+        assertNull(intQueue.dequeue());
+        assertEquals(0, intQueue.size);
     }
 }
--- src/test/java/data/structure/impl/StackTest.java
+++ src/test/java/data/structure/impl/StackTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StackTest {
 
     @Test
-    void testPushAndPop() {
+    void testPushAndPop_WithString() {
         Stack<String> stack = new Stack<>();
 
         stack.push("FIRST");
@@ -23,5 +23,26 @@ class StackTest {
 
         assertNull(stack.pop());
         assertEquals(0, stack.size);
+    }
+
+    @Test
+    void testPushAndPop_WithInteger() {
+        Stack<Integer> intStack = new Stack<>();
+
+        intStack.push(Integer.valueOf(23));
+        intStack.push(Integer.valueOf(56));
+        intStack.push(Integer.valueOf(79));
+        intStack.push(Integer.valueOf(85));
+
+        intStack.print();
+
+        assertEquals(4, intStack.size);
+        assertEquals(85, intStack.pop());
+        assertEquals(79, intStack.pop());
+        assertEquals(56, intStack.pop());
+        assertEquals(23, intStack.pop());
+
+        assertNull(intStack.pop());
+        assertEquals(0, intStack.size);
     }
 }
