31b1581c4c31ece4b05e583a0eb45ad5aff56b96
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-12-29 00:29:20 -0500

    Finding Minimum Platform Algorithm

--- src/test/java/leetcode/GeekForGeekTest.java
+++ src/test/java/leetcode/GeekForGeekTest.java
@@ -116,4 +116,72 @@ public class GeekForGeekTest {
                      .allMatch(i -> k >= i);
     }
 
+    @Test
+    public void testFindMinNumPlatforms() {
+        String[] arrival = {"9:00", "9:40", "9:50", "11:00", "15:00", "18:00" };
+        String[] departure = {"9:10", "12:00", "11:20", "11:30", "19:00", "20:00" };
+        Assertions.assertEquals(3, findMinNumPlatforms(arrival, departure));
+
+        String[] arrival2 = {"9:00", "9:40"};
+        String[] departure2 = {"9:10", "12:00"};
+        Assertions.assertEquals(1, findMinNumPlatforms(arrival2, departure2));
+    }
+
+    private int findMinNumPlatforms(String[] arrival, String[] departure) {
+        List<TrainTime> arrivalList = Arrays.stream(arrival)
+                                            .map(this::convertTrainTime)
+                                            .sorted(this::compareTime)
+                                            .toList();
+
+        List<TrainTime> departureList = Arrays.stream(departure)
+                                              .map(this::convertTrainTime)
+                                              .sorted(this::compareTime)
+                                              .toList();
+
+        int count = 0, minPlatformCount = 0;
+
+        int i = 0, j = 0;
+
+        while (i < arrivalList.size() && j < departureList.size()) {
+            TrainTime t1 = arrivalList.get(i);
+            TrainTime t2 = departureList.get(j);
+            if (compareTime(t1, t2) < 0) {
+                count++;
+                i++;
+            } else {
+                count--;
+                j++;
+            }
+
+            if(minPlatformCount < count) {
+                minPlatformCount = count;
+            }
+        }
+        return minPlatformCount;
+    }
+
+    private TrainTime convertTrainTime(String time) {
+        String[] splits = time.split(":");
+        return new TrainTime(Integer.parseInt(splits[0]), Integer.parseInt(splits[1]));
+    }
+
+    /**
+     * @param t1
+     * @param t2
+     * @return comparator in ascending order
+     * if a < b -> a - b
+     */
+    private int compareTime(TrainTime t1, TrainTime t2) {
+        if (t1.hour() < t2.hour()) return -1; //no swap
+        else if (t1.hour() > t2.hour()) return 1; //swap
+        else {
+            //t1.hour() == t2.hour()
+            if (t1.min() < t2.min()) return -1;
+            else if (t1.min() > t2.min()) return 1;
+            else return -1;
+        }
+    }
+}
+
+record TrainTime(int hour, int min) {
 }
